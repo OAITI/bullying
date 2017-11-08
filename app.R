8bd330a2d64de50b69cb601bc94b9de471058046
Eric Hare <ericrhare@gmail.com>
2017-11-08 14:21:37 -0600

    Initial commit of the bullying app

---
+++ app.R
@@ -0,0 +1,166 @@
+library(shiny)
+library(readr)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(maps)
+library(sp)
+library(rgeos)
+library(rgdal)
+library(maptools)
+library(leaflet)
+library(scales)
+library(shinythemes)
+
+addResourcePath("images", "images")
+
+bullying <- read_csv("data/bullying/bullying_clean.csv")
+iowa_districts <- readOGR(dsn = "data/cb_2016_iowa_school_districts", layer = "IdoeSchoolDistrictsFY2016", stringsAsFactors = FALSE)
+
+ui <- fluidPage(theme = shinytheme("cerulean"),
+   
+   titlePanel("Bullying Incidents in Iowa School Districts"),
+   
+   sidebarLayout(
+      sidebarPanel(
+          a(href = "https://oaiti.org", target = "_blank", img(src = "images/oaiti_transparent.png", width = "135")),
+          h4("About"),
+          HTML("This application uses data from the <a href='http://educateiowa.gov' target='_blank'>Iowa Department of Education</a> on bullying incidents in Iowa school districts. We use the district shapefiles to allow you to explore this data by clicking on the maps.<br><br>The Explore tab displays the number of incidents of a particular type in a particular year, with the option to scale by the enrollment of the district. The Model tab uses a simple linear regression to model the trend in the selected incident type over time for each district. Finally, the Data Sources tab gives information on the data collection process and displays a table of the raw results."),
+          
+          h4("Configuration"),
+          selectInput("variable", "Incidents", choices = c("All" = "Founded Incidents", names(bullying)[4:21])),
+          
+          conditionalPanel(condition = "input.tabs1 == 'Explore'",
+                           selectInput("year", "Year", choices = c("All", 2013, 2014, 2015, 2016)),
+                           checkboxInput("scale", "Scale by District Enrollment")     
+          )
+      ),
+      
+      mainPanel(
+          tabsetPanel(id = "tabs1",
+              tabPanel("Explore",
+                       h4("Map of Bullying Incidents"),
+                       HTML("The map below displays the total number of bullying incidents by district over the specified time frame. Use the inputs on the left to filter to a specific type of incident and/or a specific year. Districts colored in gray had missing data during the specified time frame."),
+                       
+                       leafletOutput("mymap")
+              ),
+              
+              tabPanel("Model",
+                       h4("Modeling the Trend"),
+                       HTML("Here we use a Simple Linear Regression to model the trend in bullying incidents between 2013 and 2016. A negative trend (colored in green) indicates that bullying incidents appear to be on the decrease, while a positive trend (colored in red) indicates an increase. Districts are colored gray where data was too limited to compute the model."),
+                       leafletOutput("modelmap")
+              ),
+              
+              tabPanel("Data Source",
+                       h4("Data Source"),
+                       HTML("This data was collected by the <a href='http://educateiowa.gov' target='_blank'>Iowa Department of Education</a>. The data has been collectd from the Excel spreadsheets on the site. Because of a change in data format in 2013, data from prior to 2013 was not used. Also note that in districts where bullying incidents were between 1 and 9 in a particular year, the exact value was redacted to preserve student privacy. We assigned a random value from 1 to 9 for these districts, and as such the values presented in this application should be treated as an approximation to the truth."),
+                       
+                       hr(),
+                       
+                       h4("Raw Data"),
+                       dataTableOutput("data")
+              )
+          )
+      )
+   )
+)
+
+server <- function(input, output) {
+    
+    mycoef <- function(formula, data) {
+        result <- try(coef(lm(formula, data = data)))
+        
+        if (inherits(result, "try-error")) return(NA)
+        
+        return(result)
+    }
+    
+    output$modelmap <- renderLeaflet({
+        withProgress(message = "Rendering model map", detail = "Please wait...", {
+            bullying$MyVar <- bullying[[input$variable]]
+            bullying_fip <- bullying %>%
+                group_by(District, Year) %>%
+                summarise(`Incidents` = sum(MyVar),
+                          `District Name` = tools::toTitleCase(`District Name`)[1]) %>%
+                group_by(`District`) %>%
+                do(Trend = mycoef(Incidents ~ Year, data = .)[2],
+                   Name = .$`District Name`[1])
+            bullying_fip2 <- data.frame(District = bullying_fip$District,
+                                        Name = unlist(bullying_fip$Name),
+                                        Trend = unlist(bullying_fip$Trend))
+            
+            leafmap <- iowa_districts %>%
+                merge(bullying_fip2, by.x = "IDOE_ID", by.y = "District")
+            
+            # Format popup data for leaflet map.
+            popup_dat <- paste0("<strong>District: </strong>", 
+                                leafmap$Name, 
+                                "<br><strong>Incident Trend: </strong>", 
+                                round(leafmap$Trend, digits = 3))
+            
+            absmax <- ceiling(max(abs(leafmap$Trend), na.rm = TRUE))
+            pal <- colorNumeric("RdYlGn", -absmax:absmax, reverse = TRUE)
+            
+            # Render final map in leaflet.
+            leaflet(data = leafmap) %>% addTiles() %>%
+                addPolygons(fillColor = ~pal(Trend), 
+                            fillOpacity = 0.8, 
+                            color = "#BDBDC3", 
+                            weight = 1,
+                            popup = popup_dat) %>%
+                addLegend(pal = pal, values = ~Trend, opacity = 1, title = "Trend in Incidents")
+        })
+    })
+    
+    output$mymap <- renderLeaflet({
+        withProgress(message = "Rendering incidents map", detail = "Please wait...", {
+            myyear <- input$year
+            if (myyear == "All") myyear <- 2013:2016
+            
+            bullying$MyVar <- bullying[[input$variable]]
+            bullying_fip <- bullying %>%
+                filter(Year %in% myyear) %>%
+                group_by(District) %>%
+                summarise(`Incidents` = ifelse(all(is.na(MyVar)), NA, sum(MyVar, na.rm = TRUE)),
+                          `District Name` = `District Name`[1],
+                          `District Enrollment` = ifelse(all(is.na(Enrollment)), NA, sum(Enrollment, na.rm = TRUE)))
+            if (input$scale) bullying_fip$Incidents = bullying_fip$Incidents * 1000 / bullying_fip$`District Enrollment`
+            
+            leafmap <- iowa_districts %>%
+                merge(bullying_fip, by.x = "IDOE_ID", by.y = "District")
+            
+            # Format popup data for leaflet map.
+            popup_dat <- paste0("<strong>District: </strong>", 
+                                leafmap$`District Name`, 
+                                "<br><strong>Incidents: </strong>", 
+                                round(leafmap$Incidents, digits = 2),
+                                "<br><strong>Enrollment: </strong>",
+                                leafmap$`District Enrollment`)
+            
+            mytitle <- ifelse(input$scale, paste(input$variable, "(per 1000)"), input$variable)
+            pal <- colorNumeric("YlOrRd", NULL)
+            
+            # Render final map in leaflet.
+            leaflet(data = leafmap) %>% addTiles() %>%
+                addPolygons(fillColor = ~pal(Incidents), 
+                            fillOpacity = 0.8, 
+                            color = "#BDBDC3", 
+                            weight = 1,
+                            popup = popup_dat) %>%
+                addLegend(pal = pal, values = ~Incidents, opacity = 1, title = mytitle)
+        })
+    })
+    
+    output$data <- renderDataTable({
+        myyear <- input$year
+        if (myyear == "All") myyear <- 2013:2016
+        
+        return(bullying %>%
+                   filter(Year %in% myyear) %>%
+                   select(District, `District Name`, Year, Enrollment, .data[[input$variable]]) %>%
+                   arrange(desc(.data[[input$variable]])))
+    })
+    
+}
+
+shinyApp(ui = ui, server = server)
---
+++ code/education_scrape.R
@@ -0,0 +1,102 @@
+###
+### Required Libraries
+### 
+library(rvest)
+library(dplyr)
+library(readxl)
+
+###
+### Student Enrollment
+###
+url <- "https://www.educateiowa.gov/education-statistics"
+my_nodes <- url %>%
+    read_html() %>%
+    html_nodes(css = "ul")
+my_links <- my_nodes[[9]] %>%
+    html_node("li") %>%
+    html_nodes("a") %>%
+    html_attr("href")
+my_links <- paste0("https:", my_links)
+
+if (!file.exists(file.path("data", "enrollment"))) dir.create(file.path("data", "enrollment"), recursive = TRUE)
+lapply(my_links, function(thelink) {
+    myhref <- thelink %>%
+        read_html() %>%
+        html_nodes(css = ".file a")
+    myfile <- myhref %>%
+        html_attr("href")
+    myname <- myhref %>%
+        html_text
+    
+    download.file(myfile, file.path("data", "enrollment", myname), mode = "wb")
+})
+
+enrollment_2016 <- read_excel("data/enrollment/2015-2016 Iowa Public School District PreK-12 Enrollments by District, Grade, Race and Gender.xlsx", skip = 4) %>%
+    select(`COUNTY NAME`, `DISTRICT NUMBER`, `Total K12`)
+enrollment_2015 <- read_excel("data/enrollment/2015-2016 Iowa Public School District PreK-12 Enrollments by District, Grade, Race and Gender.xlsx", skip = 4) %>%
+    select(`COUNTY NAME`, `DISTRICT NUMBER`, `Total K12`)
+enrollment_2014 <- read_excel("data/enrollment/2015-2016 Iowa Public School District PreK-12 Enrollments by District, Grade, Race and Gender.xlsx", skip = 4) %>%
+    select(`COUNTY NAME`, `DISTRICT NUMBER`, `Total K12`)
+enrollment_2013 <- read_excel("data/enrollment/2015-2016 Iowa Public School District PreK-12 Enrollments by District, Grade, Race and Gender.xlsx", skip = 4) %>%
+    select(`COUNTY NAME`, `DISTRICT NUMBER`, `Total K12`)
+enrollment <- rbind(enrollment_2013, enrollment_2014, enrollment_2015, enrollment_2016)
+enrollment$Year <- rep(2013:2016, times = c(nrow(enrollment_2013), nrow(enrollment_2014), nrow(enrollment_2015), nrow(enrollment_2016)))
+enrollment$`COUNTY NAME`[enrollment$`COUNTY NAME` == "Pottawattam"] <- "Pottawattamie"
+
+###
+### Bullying
+###
+my_links <- url %>%
+    read_html() %>%
+    html_nodes(css = "#BullyingData~ a") %>%
+    html_attr("href")
+my_links <- paste0("https:", my_links)
+
+if (!file.exists(file.path("data", "bullying"))) dir.create(file.path("data", "bullying"), recursive = TRUE)
+lapply(my_links, function(thelink) {
+    myhref <- thelink %>%
+        read_html() %>%
+        html_nodes(css = ".file a")
+    myfile <- myhref %>%
+        html_attr("href")
+    myname <- myhref %>%
+        html_text
+    
+    download.file(myfile, file.path("data", "bullying", myname), mode = "wb")
+})
+
+###
+### Joining Bullying with County
+###
+bullying_2016 <- read_excel("data/bullying/2015-2016BullingData.xlsx", sheet = 2, skip = 10)
+names(bullying_2016)[2] <- "District Name"
+names(bullying_2016)[3] <- "Founded Incidents"
+
+bullying_2015 <- read_excel("data/bullying/2014-2015BullingData.xlsx", sheet = 2, skip = 10)
+names(bullying_2015)[2] <- "District Name"
+names(bullying_2015)[3] <- "Founded Incidents"
+
+bullying_2014 <- read_excel("data/bullying/2013-2014BullyingData.xlsx", sheet = 2, skip = 9)
+names(bullying_2014)[2] <- "District Name"
+names(bullying_2014)[3] <- "Founded Incidents"
+bullying_2014$`Bully - Other` <- NA
+
+bullying_2013 <- read_excel("data/bullying/2012-2013BullyingData.xlsx", sheet = 2, skip = 9)
+names(bullying_2013)[2] <- "District Name"
+names(bullying_2013)[43] <- "Bully - Other"
+
+bullying_all <- rbind(bullying_2016, bullying_2015, bullying_2014, bullying_2013)
+bullying_all$Year <- rep(2016:2013, times = c(nrow(bullying_2016), nrow(bullying_2015), nrow(bullying_2014), nrow(bullying_2013)))
+#bullying_all$`District Name`[bullying_all$`District Name` == "Decorah Community"] <- "Decorah"
+
+bullying_join <- bullying_all %>%
+    mutate(District = as.numeric(District)) %>%
+    left_join(enrollment, by = c("District" = "DISTRICT NUMBER", "Year" = "Year")) %>%
+    filter(!duplicated(interaction(District, Year)),
+           `District Name` != "State") %>%
+    select(District:Other, Year, County = `COUNTY NAME`, Enrollment = `Total K12`)
+#bullying_join[is.na(bullying_join)] <- 0
+bullying_join$`Founded Incidents`[bullying_join$`Founded Incidents` == "<10" & !is.na(bullying_join$`Founded Incidents`)] <- sample(1:9, size = sum(bullying_join$`Founded Incidents` == "<10", na.rm = TRUE), replace = TRUE)
+bullying_join[bullying_join == "<10"] <- 0
+write.csv(bullying_join, file = "data/bullying/bullying_clean.csv", row.names = FALSE)
+
